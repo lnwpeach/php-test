3d239c9c61ebca3b8ed9db837dc0aec8543f9c2e
peach <pichayuth.c@gmail.com>
2020-10-06 21:29:29 +0700

    cron test 2020-10-06 21:27:49

--- cron/cron-test.php
+++ cron/cron-test.php
@@ -4,15 +4,14 @@ $dir = __DIR__;
 $myfile = fopen(__DIR__."/cron-text.txt", "w") or die("Unable to open file!");
 $txt = "Test crontab\n";
 fwrite($myfile, $txt);
-$dt = date("Y-m-d H:i:s")."\n";
+$dt = date("Y-m-d H:i:s");
 fwrite($myfile, $dt);
 fclose($myfile);
 
-echo $dir;
-// exec("git -C {$dir}/.. add -A");
-// exec("git -C {$dir}/.. commit -m 'cron test {$dt}'");
-// exec("git -C {$dir}/.. push github master");
+exec("git -C {$dir}/.. add -A");
+exec("git -C {$dir}/.. commit -m 'cron test {$dt}'");
+exec("git -C {$dir}/.. push github master");
 
-// echo 'Success';
+echo 'Success';
 
 ?>
--- cron/write-file.php
+++ cron/write-file.php
@@ -2,7 +2,7 @@
 $myfile = fopen(__DIR__."/cron-text.txt", "w") or die("Unable to open file!");
 $txt = "Test crontab\n";
 fwrite($myfile, $txt);
-$dt = date("Y-m-d H:i:s")."\n";
+$dt = date("Y-m-d H:i:s");
 fwrite($myfile, $dt);
 fclose($myfile);
 ?>
