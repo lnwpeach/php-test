0d2db6664dc922e9c4efe2d90578b7a8c2c0350c
Peach <pichayuth.c@gmail.com>
2020-10-06 17:02:14 +0700

    update, gen pass

--- index.php
+++ index.php
@@ -10,8 +10,10 @@ echo "<br><br>";
 
 echo '<h3>Test</h3>';
 
-$aa = strtolower("THB");
-echo "aa: ".$aa;
+echo uniqid('', true).'<br>';
+echo uniqid('', true).'<br>';
+echo uniqid('', true).'<br>';
+echo uniqid('', true).'<br>';
 echo "<br><br>";
 
 $revise = ['page' => 1, 'per_page' => 50];
@@ -44,15 +46,59 @@ echo 'sha512: '.$aa.'<br>';
 echo '<hr>';
 
 echo '<h3>function crypt</h3>';
-echo 'microtime: '.microtime(true).'<br>';
-
-$ranid = uniqid('', true);
+echo '$5$: sha256<br>';
+echo '$6$: sha512<br>';
+$st = microtime(true);
+$ranid = substr(uniqid('', true), 6);
 echo 'uniqid: '.$ranid.'<br>';
-$salt = '$5$rounds=5043$'.$ranid;
+
+echo '<br>';
+echo 'sha256<br>';
+$salt = '$5$rounds=8167$'.$ranid;
+echo 'salt: '.$salt.'<br>';
 $aa = crypt('1234', $salt);
 echo $aa.'<br>';
 $bb = crypt('1234', $aa);
 echo $bb.'<br>';
-echo 'microtime: '.microtime(true).'<br>';
+echo '<br>';
+
+echo '<br>';
+echo 'sha512<br>';
+$salt = '$6$rounds=8167$'.$ranid;
+echo 'salt: '.$salt.'<br>';
+$aa = crypt('1234', $salt);
+echo $aa.'<br>';
+$bb = crypt('1234', $aa);
+echo $bb.'<br>';
+
+$ed = microtime(true);
+$time = ($ed - $st) * 1000;
+echo 'time: '.($time).' ms<br>';
+echo '<hr>';
+
+echo '<h3>Time</h3>';
+echo 'time: '.time();
+echo '<br>';
+echo 'microtime: '.microtime(true);
+echo '<br>';
+echo '<hr>';
+
+echo '<h3>ASCII</h3>';
+echo 'ord A: '.ord('A');
+echo '<br>';
+echo 'chr 65: '.chr(65);
+echo '<br>';
+echo '<hr>';
+
+echo '<h3>Base number</h3>';
+echo 'dexhex 10: '.dechex('10');
+echo '<br>';
+echo 'hexdec a: '.hexdec('a');
+echo '<br>';
+echo 'decbin 95: '.decbin('95');
+echo '<br>';
+echo 'bindec 1011111: '.bindec('1011111');
+echo '<br>';
+echo '<hr>';
 
 ?>
