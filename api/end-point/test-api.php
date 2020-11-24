7848a61a6fb24fc75d2e1233dd4218e8bf7564f6
Peach <pichayuth.c@gmail.com>
2020-11-24 11:53:49 +0700

    test-api

--- api/end-point/test-api.php
+++ api/end-point/test-api.php
@@ -2,6 +2,7 @@
 header('Access-Control-Allow-Origin: *');
 
 $answer['success'] = 1;
+$answer['get'] = $_GET;
 $answer['post'] = $_POST;
 $answer['input'] = file_get_contents('php://input');
 exit(json_encode($answer));
